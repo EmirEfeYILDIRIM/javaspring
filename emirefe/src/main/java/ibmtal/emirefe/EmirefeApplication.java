df6df69f1f8fc67dc7d60df49d0d2d072ac6e7dc
EmirEfeYILDIRIM <113903260+EmirEfeYILDIRIM@users.noreply.github.com>
2022-11-21 16:23:18 +0300

    Initial commit

---
+++ emirefe/src/main/java/ibmtal/emirefe/EmirefeApplication.java
@@ -0,0 +1,13 @@
+package ibmtal.emirefe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmirefeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmirefeApplication.class, args);
+	}
+
+}
